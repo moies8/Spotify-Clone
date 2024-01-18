b06cb01b97f00f756deca1428a8211415bc1c77f
Moies Malik <moiesmalik8@gmail.com>
2024-01-18 23:57:18 +0530

    Enhanced Songlist Styling and Added Dynamic Song Display
    Today's update includes enhancements to the Spotify Clone's visual elements and introduces dynamic song display:
    
    CSS Styling Additions:
    
    Added styling for the '.songlist' section with improved aesthetics.
    Styled individual song list items ('.songlist ul li') for a more user-friendly display.
    
    Included hover effects for better user interaction.
    
    JavaScript Dynamic Display:
    
    Added a script in 'script.js' to dynamically display songs in the songlist.
    Songs are now displayed within the styled list ('.songlist ul li').
    Visual Improvements:
    
    Adjusted spacing, background colors, and font sizes to improve overall visual appeal.
    These changes refine the Spotify Clone's appearance and provide dynamic song display, enhancing the user experience. More updates coming soon! 🎵🚀 #SpotifyClone #WebDevelopment #VisualEnhancements"

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 
 
 
-async function getsongs(){
+async function getsongs() {
     let songs = await fetch("http://127.0.0.1:3000/songs/")
-    let response= await songs.text();
+    let response = await songs.text();
     let div = document.createElement("div")
     div.innerHTML = response;
     let as = div.getElementsByTagName('a')
     let mysongs = []
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
-        if(element.href.endsWith('.mp3')){
-            mysongs.push(element.href)
-        }   
+        if (element.href.endsWith('.mp3')) {
+            mysongs.push(element.href.split("/songs/")[1])
+        }
     }
     return mysongs
 }
 
 
-async function main(){
+async function main() {
     let songs = await getsongs()
     console.log(songs)
-    let songUl= document.querySelector('.songlist').getElementsByTagName('li')[0]
+    let songUl = document.querySelector('.songlist').getElementsByTagName('ul')[0]
     // songUl.innerHTML= songUl.innerHTML + songs
     for (const song of songs) {
-        songUl.innerHTML= songUl.innerHTML + song
-        
+        songUl.innerHTML = songUl.innerHTML + `<li>${song.replaceAll("%20", " ")}</li>`;
+
     }
     let audio = new Audio(songs[0])
     // audio.play();
-    audio.addEventListener('loadeddata' ,function(){
+    audio.addEventListener('loadeddata', function () {
         console.log(audio.duration)
     })
-    
+
 }
 
 main()
